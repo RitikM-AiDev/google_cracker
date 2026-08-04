db2a7aa79f8738a507123b60c4ca6b28cbddde0f
RitikM-AiDev <ritik.aidev@gmail.com>
2026-08-04 22:05:32 +0530

    lc 135

---
+++ lc3731.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        l = sorted(nums,reverse=True)
+        res=[]
+        v=l.pop()
+        while l:
+            v+=1
+            if v==l[-1]:
+                l.pop()
+            else:
+                res.append(v)
+        return res
